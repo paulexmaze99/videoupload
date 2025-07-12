d7ee9d5c80acbd365bb6b39b631dbabc5051f058
raph wrld <104195630+paulexmaze99@users.noreply.github.com>
2025-07-12 16:57:34 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sqlX.freesqldatabase.com';  // Replace with your host
+$host = 'sql8.freesqldatabase.com';  // Replace with your host
 $port = 3306;
 $user = 'sql8789664';                // From email
-$pass = 'DRRxv9gR#u44.DF';             // From email
+$pass = 'EzAcq4CUXs';             // From email
 $db   = 'sql8789664';                // DB name from email
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);

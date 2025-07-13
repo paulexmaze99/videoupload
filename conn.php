9308c512cc8f747474c118706295d7c9a42cbf14
raph wrld <paulokolie99@gmail.com>
2025-07-13 18:01:44 +0100

    Update database connection settings in conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$host = '127.0.0.1';
-$port = '3308'; // Default DBngin port
-$user = 'root';
-$pass = '';     // Add password if set in DBngin
-$dbname = 'db_video';
+$host = 'db.pxxl.pro';
+$port = 40796;
+$user = 'user_be284063';
+$pass = '04f62ad14e6aa7f49d7f8bc15c566f2e';
+$dbname = 'db_6ed624af';
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 

546e9728f4910480b49b1ba2a57c0dffbb0c0445
raph wrld <paulokolie99@gmail.com>
2025-07-13 18:27:21 +0100

    Switched to FreeSQLDatabase remote MySQL

--- conn.php
+++ conn.php
@@ -1,11 +1,21 @@
 <?php
-$host = 'db.pxxl.pro';
-$port = 40796;
-$user = 'user_be284063';
-$pass = '04f62ad14e6aa7f49d7f8bc15c566f2e';
-$dbname = 'db_6ed624af';
+$host = 'sql8.freesqldatabase.com';
+$port = 3306;
+$user = 'sql8789664';
+$pass = 'EzAcq4CUXs';
+$dbname = 'sql8789664';
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+$attempts = 0;
+$maxAttempts = 5;
+
+do {
+    $conn = @new mysqli($host, $user, $pass, $dbname, $port);
+    if ($conn && !$conn->connect_error) {
+        break;
+    }
+    $attempts++;
+    sleep(2); // Wait before retrying
+} while ($attempts < $maxAttempts);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
